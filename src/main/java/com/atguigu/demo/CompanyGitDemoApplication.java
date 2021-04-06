f4f864540b72cdcad96d29b4ca9feec63fb1c5f8
sunjia <88591@qq.com>
2021-04-06 18:02:54 +0800

    第一次提交

---
+++ src/main/java/com/atguigu/demo/CompanyGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyGitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyGitDemoApplication.class, args);
+    }
+
+}
